44b862e21532618ad1e61d5c7fa37aa74f78fc30
Mylo <36931363+gitmylo@users.noreply.github.com>
2021-09-16 20:02:35 +0200

    add title to example page

--- WpfApp/PHPinApp/bin/Debug/htdocs/index.php
+++ WpfApp/PHPinApp/bin/Debug/htdocs/index.php
@@ -1,4 +1,9 @@
+<head>
+<title>PHPinApp</title>
+</head>
+<body>
 <h1>Welcome, this webserver is running locally though the app!</h1>
 <h4>edit the config.ini to change the settings for the program</h4>
 a link to the project's <a href="https://github.com/gitmylo/PHPinApp">Github</a><br>
 you can use html, css, js and of course php. happy coding!
+</body>
